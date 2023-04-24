fd17fd504d8d82297c045c25f02a7fd9aec5eedf
Omar Saied <74738267+omarsa3ied@users.noreply.github.com>
2023-04-24 14:26:06 +0200

    .

--- App.py
+++ App.py
@@ -57,27 +57,96 @@ def plot_wav_file(uploaded_file, color):
         # Calculate the duration of the file
         duration = len(data) / sample_rate
 
-        # Create a spectrogram
-        freqs, times, spectrogram = signal.spectrogram(data, sample_rate)
-
         # Create a time axis
         time = np.linspace(0, duration, len(data))
 
         # Create a plot
-        fig, ax = plt.subplots(1, 1)
-
-        ax.plot(time, data, 'b-', linewidth=3)
-        ax.grid()
-        ax.set_facecolor(ax.get_facecolor())
+        fig, ax = plt.subplots(1,1,figsize=(8, 4))
+        ax.plot(time, data, color=color, linewidth=1.5, linestyle='-')
 
         # Set the plot title and labels
-        font1 = {'family': 'serif', 'color': 'white', 'size': 20}
-        ax.set_xlabel("Time (s)", fontsize=12, fontdict=font1)
-        ax.set_ylabel("Amplitude", fontsize=12, fontdict=font1)
-
+        ax.set_title("Uploaded WAV file")
+        ax.set_xlabel("Time (s)", fontsize=12)
+        ax.set_ylabel("Amplitude", fontsize=12)
+        
         # Display the plot
-        line = ax.plot(time, data, color=color, linewidth=3)[0]
-        st.plotly_chart(fig, use_container_width=True)
+        st.pyplot(fig)
+        # Create an audio player widget
+        with io.BytesIO() as wav_file:
+            wav_writer = wave.open(wav_file, "wb")
+            wav_writer.setnchannels(1)
+            wav_writer.setsampwidth(2)
+            wav_writer.setframerate(sample_rate)
+            wav_writer.writeframes(data)
+            wav_writer.close()
+            st.audio(wav_file.getvalue(), format='audio/wav')
+        
+        # plotting the spectrogram
+        plot_spectrogram(uploaded_file)
+
+
+def apply_filters(data, sample_rate, freq_range, slider_values):
+    # Calculate the number of samples in the data
+    n_samples = len(data)
+
+    # Compute the Fourier transform of the data
+    data_ft = np.fft.rfft(data)
+
+    # Create an array of frequencies for each frequency bin
+    freqs = np.fft.rfftfreq(n_samples, d=1/sample_rate)
+
+    # Initialize an empty array to store the modified frequency components
+    modified_freqs = np.zeros_like(data_ft)
+
+    # Apply the frequency filters to the data
+    for i in range(10):
+        # Determine the frequency range for the current slider
+        min_freq = i * (freq_range / 10)
+        max_freq = (i + 1) * (freq_range / 10)
+
+        # Determine the indices of the frequency components within the current range
+        freq_indices = np.where((freqs >= min_freq) & (freqs < max_freq))[0]
+
+        # Apply the current slider value to the frequency components within the current range
+        slider_value = slider_values[i]
+        modified_freqs[freq_indices] = slider_value * data_ft[freq_indices]
+
+    return modified_freqs
+
+
+def uniform_range_mode(uploaded_file,color):
+    # Check if a file was uploaded
+    if uploaded_file is not None:
+        # Load the file data
+        sample_rate, data = wavfile.read(uploaded_file)
+
+        # Calculate the frequency range of the signal
+        freq_range = sample_rate / 2
+
+        # Calculate the frequency range for each slider
+        slider_range = freq_range / 10
+
+        # Create an empty list to store the slider values
+        slider_values = []
+
+        # Create a slider for each frequency range
+        for i in range(10):
+            slider = st.sidebar.slider(f"Frequency Range {i+1}", 0, 100, 50)
+            slider_values.append(slider)
+
+        # Apply the frequency filters to the signal
+        modified_freqs = apply_filters(data, sample_rate, freq_range, slider_values)
+
+        # Compute the inverse Fourier transform of the modified frequency components
+        reconstructed_signal = np.fft.irfft(modified_freqs)
+
+        # Create a plot of the reconstructed signal
+        fig, ax = plt.subplots(1, 1, figsize=(8, 4))
+        ax.plot(reconstructed_signal,color=color, linewidth=1.5, linestyle='-')
+        ax.set_xlabel("Time (samples)", fontsize=12)
+        ax.set_ylabel("Amplitude", fontsize=12)
+        ax.set_title("Reconstructed Signal", fontsize=12)
+        st.pyplot(fig)
 
         # Create an audio player widget
         with io.BytesIO() as wav_file:
@@ -89,19 +158,19 @@ def plot_wav_file(uploaded_file, color):
             wav_writer.close()
             st.audio(wav_file.getvalue(), format='audio/wav')
 
-        
-        # Define the function to update the plot in real-time
-        def update_plot(frame):
-            line.set_ydata(data[frame])
-            return line,
-
-        # Define the animation
-        frames = len(data)
-        interval = 1000 / sample_rate
-        animation = FuncAnimation(fig, update_plot, frames=frames, interval=interval, blit=True)
-        
-        # plotting the spectrogram
-        plot_spectrogram(uploaded_file)
+        # Create a plot of the spectrogram
+        fig, ax = plt.subplots(1, 1, figsize=(8, 4))
+        # Normalize the values in the spectrogram array
+        spectrogram, freqs, bins, im = ax.specgram(data, Fs=sample_rate, cmap='viridis')
+        # Set x and y limits of the spectrogram plot
+        ax.set_xlim([0, len(data)/sample_rate])
+        ax.set_ylim([0, freq_range])
+        # Set the labels for the plot
+        ax.set_xlabel("Time (s)", fontsize=12)
+        ax.set_ylabel("Frequency (Hz)", fontsize=12)
+        ax.set_title("Spectrogram", fontsize=12)
+        # Display the plot
+        st.pyplot(fig)
 
 
 
@@ -127,20 +196,7 @@ with leftCol:
 
     plot_wav_file(uploadedFile,color1)
 
+with rightCol:
+    if selected_option == "Uniform Range Mode":
+        uniform_range_mode(uploadedFile,color2)
 
-if uploadedFile:
-    st.write("<p style='font-size:20px;'>Adjust the parameters:</p>", unsafe_allow_html=True)
-    cols = st.columns(10)
-
-    for i in range(10):
-        with cols[i%10]:
-            st.write(f"<h3>Slider {i+1}</h3>", unsafe_allow_html=True)
-            svs.vertical_slider(
-            key=f"slider_{i}",
-            default_value=50,
-            min_value=0,
-            max_value=100,
-            step=1,
-            slider_color="blue",
-            track_color="lightgray",
-            thumb_color="red",)
